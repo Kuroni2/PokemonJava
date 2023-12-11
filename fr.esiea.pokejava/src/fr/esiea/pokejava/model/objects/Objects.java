e41099205c3b0e7874744478df336fb548c1f57a
marcellinvianney <145564428+marcellinvianney@users.noreply.github.com>
2023-12-11 15:46:19 +0100

    battle

--- fr.esiea.pokejava/src/fr/esiea/pokejava/model/objects/Objects.java
+++ fr.esiea.pokejava/src/fr/esiea/pokejava/model/objects/Objects.java
@@ -2,8 +2,14 @@ package fr.esiea.pokejava.model.objects;
 
 public abstract class Objects {
     String nom;
-    public Objects(String nom) {
+    String type;
+    public Objects(String nom, String type) {
         this.nom = nom;
+        this.type = type;
+    }
+    void use(Monster target) {
+        // Logique d'utilisation de l'objet sur le monstre
+        System.out.println("Utilisation de l'objet " + name + " sur " + target.name);
     }
     public abstract void utiliser();
 }
