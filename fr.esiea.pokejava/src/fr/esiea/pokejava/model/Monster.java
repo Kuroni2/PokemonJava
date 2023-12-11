4765119e6fcd5a02b24e2a1e072751cadc9482e2
Nathan <nchretien@et.esiea.fr>
2023-12-11 09:32:35 +0100

    model package

---
+++ fr.esiea.pokejava/src/fr/esiea/pokejava/model/Monster.java
@@ -0,0 +1,16 @@
+package fr.esiea.pokejava.model;
+
+public class Monster {
+
+    private String name;
+
+    private int hp;
+
+    private int attack;
+
+    private int defense;
+
+    private int speed;
+
+    public Monster(String )
+}
