22d2d65e242c3ae90d59ecf05a360dc8898451dc
Nathan <nchretien@et.esiea.fr>
2023-12-11 13:54:03 +0100

    jbhjvjjn bnb

--- fr.esiea.pokejava/src/fr/esiea/pokejava/model/objects/Objects.java
+++
@@ -1,9 +0,0 @@
-package fr.esiea.pokejava.model.objects;
-
-public abstract class Objects {
-    String nom;
-        public Object(String nom) {
-            this.nom = nom;
-        }
-        abstract void utiliser();
-}
